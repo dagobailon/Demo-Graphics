2900532a38c8075cc369f66f9e1e82e2e7f0c53c
dagobailon <dago@codeforprogress.org>
2014-05-20 14:37:27 -0400

    Added Heroku files

--- app/views.py
+++ app/views.py
@@ -6,3 +6,11 @@ from flask import render_template
 @app.route('/')
 def index():
 	return render_template('DemoGraphics.html')
+
+@app.route('/DemoGraphics_2')
+def DemoGraphics_2():
+	return render_template('DemoGraphics_2.html')
+
+@app.route('/DemoGraphics_3')
+def DemoGraphics_3():
+	return render_template('DemoGraphics_3.html')
