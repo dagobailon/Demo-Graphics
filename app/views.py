f7d72d3a25fd46f73d53323897feafe5e37306b5
dagobailon <dago@codeforprogress.org>
2014-05-23 16:32:29 -0400

    New and Awesome changes

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def index():
+def DemoGraphics():
 	return render_template('DemoGraphics.html')
 
 @app.route('/DemoGraphics_2')
@@ -14,3 +14,11 @@ def DemoGraphics_2():
 @app.route('/DemoGraphics_3')
 def DemoGraphics_3():
 	return render_template('DemoGraphics_3.html')
+
+@app.route('/survey')
+def survey():
+	return render_template('survey.html')
+
+@app.route('/member')
+def member():
+	return render_template('member.html')
